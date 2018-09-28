e4bac2e24f8bb9994d08fa9a73be9f0b1aff9d9e
Jesua Sagastume <jesuasagas@gmail.com>
2018-09-27 23:43:01 -0600

    Se modifica vista de cargas y se agregan cargas para el resto de archivos, aún esperando por datos a cargar desde ER

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/teacherTest/main.css') }}" rel="stylesheet">
 
 
     {{--incluyendo librerias de botstrap y jquery--}}
