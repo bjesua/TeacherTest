3a1ac2befb3cd8e1d51e067b5a24542ee80fd8b0
Jesua Sagastume <jesuasagas@gmail.com>
2018-10-04 16:54:22 -0600

    Cambios de modelos

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
+namespace App;
+use DB;
+
+use App\Http\Requests;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+use App\Carrera as carrera;
+use App\Models\\md_estado AS md_estado;
 
 class TestController extends Controller
 {
     //
 
-
+    $queryCarrera = carrera:get
 
 
 }
